22cb3d2780b0a5d52bc1aae8ded7bf2b0050782a
Juan Maria Gonzalez Cornejo <jmcornejo@smsargentina.com.ar>
2021-12-22 10:42:01 -0300

    Segundo Commit del 2do y 3er Doc

---
+++ Tercer Documento.java
@@ -0,0 +1,36 @@
+package Mod3;
+
+public class Documento {
+	private String tipoDocumento;
+	private int numeroDocumento;
+	
+	public Documento(String tipoDocumento, int numeroDocumento) {
+		this.tipoDocumento = tipoDocumento;
+		this.numeroDocumento = numeroDocumento;
+	}
+
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+	public int getNumeroDocumento() {
+		return numeroDocumento;
+	}
+
+	public void setNumeroDocumento(int numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+
+	@Override
+	public String toString() {
+		return "Documento [tipoDocumento=" + tipoDocumento + ", numeroDocumento=" + numeroDocumento + "]";
+	}
+	
+	
+	
+	
+}
